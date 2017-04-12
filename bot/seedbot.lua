fcb92a47d91189dc82954fd540424eaf80db1bb9
pwvgaz <alizangi27@gmail.com>
2017-04-12 16:58:21 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {162998208},--Sudo users
+    sudo_users = {366937830},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
@@ -230,9 +230,6 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
 
 Special thanks to
 awkward_potato
